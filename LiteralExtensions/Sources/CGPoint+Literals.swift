ecd5d6e143f9c4c2d45662991ff94e8561c3681b
Codesworth <codezworth@gmail.com>
2019-02-18 11:30:09 +0000

    Added CGPoint Literal Init

--- LiteralExtensions/Sources/CGPoint+Literals.swift
+++ LiteralExtensions/Sources/CGPoint+Literals.swift
@@ -10,4 +10,42 @@ import Foundation
 
 
 
-extension CGPoint:ExpressibleByArrayLiteral
+extension CGPoint:ExpressibleByArrayLiteral{
+    
+    public typealias ArrayLiteralElement = CGFloat
+    
+    public init(arrayLiteral elements: CGFloat...) {
+        if elements.count > 1{
+            self = CGPoint(x: elements.first!, y: elements.last!)
+        }else{
+            if elements.isEmpty{
+                self = CGPoint.zero
+            }else{
+                self = CGPoint(x: elements.first!, y: elements.first!)
+            }
+        }
+    }
+}
+
+
+extension CGPoint:ExpressibleByIntegerLiteral{
+    
+    public typealias IntegerLiteralType = Int
+    
+    public init(integerLiteral value: IntegerLiteralType) {
+        
+        self = CGPoint(x: value, y: value)
+    }
+}
+
+
+extension CGPoint:ExpressibleByFloatLiteral{
+    
+    public typealias FloatLiteralType = Double
+    public init(floatLiteral value: FloatLiteralType) {
+        self = CGPoint(x: value, y: value)
+    }
+}
+
+
+extension CGPoint
