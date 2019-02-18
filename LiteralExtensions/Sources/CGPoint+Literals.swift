ad2eb32884a7ba6ba4111979da672ce2f283020b
Codesworth <codezworth@gmail.com>
2019-02-18 11:17:21 +0000

    Initial Commit

---
+++ LiteralExtensions/Sources/CGPoint+Literals.swift
@@ -0,0 +1,13 @@
+//
+//  CGPoint+Literals.swift
+//  LiteralExtensions
+//
+//  Created by Shadrach Mensah on 18/02/2019.
+//  Copyright © 2019 Shadrach Mensah. All rights reserved.
+//
+
+import Foundation
+
+
+
+extension CGPoint:ExpressibleByArrayLiteral
